557d02d8dc24690db967dc44798873614cf302c5
Godru <www.godrus@gmail.com>
2019-02-13 16:14:20 +0300

    first

---
+++ src/Components/box_1.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class box_1 extends Component {
+  constructor(){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div style ='background-image: url("./img/tab_3.png")'/>
+
+    );
+  }
+}
+
+export default box_1;
---
+++ src/Components/box_2.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+export default function box_2() {
+    return (
+      <div className="box_2"/>
+    );
+}
+
+

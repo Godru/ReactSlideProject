8de41594a355409aee038e91e3e65b067a329477
Godru <www.godrus@gmail.com>
2019-04-04 00:10:30 +0300

    product

---
+++ build/precache-manifest.593b88a478d582ed53326427d0ef801e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe1eacfd903ffc4bcfc56fa9a39be51e",
+    "url": "./static/media/iceblock4.fe1eacfd.png"
+  },
+  {
+    "revision": "112b6cb797bdf9c3355c",
+    "url": "./static/css/main.1011e3f5.chunk.css"
+  },
+  {
+    "revision": "eb1e0ac1913c39e2d8f9",
+    "url": "./static/js/1.eb1e0ac1.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ab27ca4504f24a6496342842ba61e90a",
+    "url": "./static/media/iceblock2.ab27ca45.png"
+  },
+  {
+    "revision": "b332cf54ac9406298da2742297aeb97c",
+    "url": "./static/media/down_button.b332cf54.png"
+  },
+  {
+    "revision": "ec56bf99fe3260c2831b6ab39daf89c9",
+    "url": "./static/media/iceblock1.ec56bf99.png"
+  },
+  {
+    "revision": "112b6cb797bdf9c3355c",
+    "url": "./static/js/main.112b6cb7.chunk.js"
+  },
+  {
+    "revision": "a8bae109c92c6217db8ed6774f99463d",
+    "url": "./static/media/box_1.a8bae109.png"
+  },
+  {
+    "revision": "7433757777de57e73d16909c8d9dbb25",
+    "url": "./static/media/tab_2.74337577.png"
+  },
+  {
+    "revision": "0e85934274fb5946345cbe5c7e6a9341",
+    "url": "./static/media/box_2.0e859342.png"
+  },
+  {
+    "revision": "db9c56ac1031f0804a21fce506751350",
+    "url": "./static/media/tab_1.db9c56ac.png"
+  },
+  {
+    "revision": "71d04ecc25bb849a1065f675d7cf7a4f",
+    "url": "./static/media/tab_3.71d04ecc.png"
+  },
+  {
+    "revision": "864ae199c0f32e8e05357c2019fe0984",
+    "url": "./index.html"
+  }
+];
